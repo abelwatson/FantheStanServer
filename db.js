2c733881107485e92412b16e1bed1c39329f3754
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-09-30 18:51:27 -0400

    fixed my app to index

--- db.js
+++ db.js
@@ -2,13 +2,8 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize( 
-    process.env.DB_DBNAME,
-    process.env.DB_USER,
-    process.env.DB_PASS,
-    {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-});
+    process.env.DATABASE_URL,
+    {dialect: 'postgres',});
 
 
 
