3983d1d709527812c00a3779646ad9f0baa9efca
Abel Watson <80369480+abelwatson@users.noreply.github.com>
2021-10-05 11:09:18 -0400

    added readme and updated code

--- db.js
+++ db.js
@@ -2,10 +2,9 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize( 
-    process.env.DATABASE_URL,
-    {dialect: 'postgres',});
-
-
-
+    process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        ssl: process.env.ENVIRONMENT === 'production'
+});
 
 module.exports = { sequelize };
